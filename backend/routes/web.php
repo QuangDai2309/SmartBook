d55f06b071c97c2ef2a52ba5c87b4dc3c37ea730
QuangDai2309 <daitdqps38672@gmail.com>
2025-06-21 11:46:38 +0700

    Update web.php

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -3,7 +3,6 @@
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\Admin\AuthorController;
-use App\Http\Controllers\Admin\BannerController;
 use App\Http\Controllers\Admin\BookCategoryController;
 use App\Http\Controllers\Admin\BookController;
 use App\Http\Controllers\Admin\BookViewController;
